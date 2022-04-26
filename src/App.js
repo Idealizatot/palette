dcffd8e852e8625ccbea5771039944f14d42cf66
Dmitrii <chamanix@Dmitriis-MacBook-Air.local>
2022-04-26 13:37:33 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { EnergyBall } from "./components/EnergyBall/EnergyBall";
+
+function App() {
+  return (
+    <div className="App">
+      <EnergyBall></EnergyBall>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EnergyBall/EnergyBall.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from './styles.module.css';
+
+const EnergyBall = () => {
+   
+const colors = ['red', 'green', 'blue', 'lime', 'yellow', 'gray'];
+   
+    return (
+    <section className={styles.section}>
+        {Array.from({length: 4}).map((item, i) => {
+            const size = 48 + Math.floor(Math.random() * i * 5) + '%';
+            return (
+                <span className={styles.sphere} style={
+                    {   
+                        backgroundColor: colors[i],
+                        transformOrigin: Math.floor(Math.random() * i * 8) + 37 + '%',
+                        animationDuration: Math.floor(Math.random() * i * 5),
+                        width: size,
+                        height: size,
+                    }
+                }/>
+            )
+        })}
+    </section>
+)};
+
+export { EnergyBall };
